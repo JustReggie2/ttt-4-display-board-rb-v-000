caddc755a3480da95776afc0600ecf6ab66b7e53
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-05-06 18:43:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,7 +12,7 @@ board = ["   ","   ","   ","   "," X ","   ","   ","   ","   "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   | #{board[4]}  |   "
+  puts "   | #{board[4]} |   "
   puts "-----------"
   puts "   |   |   "
 end
